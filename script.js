2d93115c527fdfa090f756af4f8348111e3b15bc
gabrielfaria09 <gabrielfaria0905@gmail.com>
2026-09-10 21:02:18 -0300

    Adiciona funcionalidade de busca de Pokémons e exibição de informações no card.

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
 const card_pokemon = document.getElementById("card_pokemon");
 const pesquisa = document.getElementById("pesquisa");
 const botao = document.getElementById("botao");
+const imagemPokemon = document.getElementById("ImagemPokemon");
+const paragrafoNome = document.getElementById("paragrafoNome");
+const paragrafoId = document.getElementById("paragrafoId");
+
 
 async function buscar() {
     try {
+        paragrafoNome.innerHTML = "";
+        paragrafoId.innerHTML = "";
+        imagemPokemon.setAttribute('src', "");
+
         const nome = pesquisa.value;
         const endereco = `https://pokeapi.co/api/v2/pokemon/${nome}`
 
         const requisicao = await fetch(endereco);
-        await requisicao.json();
 
-        console.log(requisicao);
-        alert(requisicao);
+        if(!requisicao.ok){
+            paragrafoNome.innerHTML = `O pokemón não exite, ou a conexão com a internet está fraca !`;
+            throw new Error("Pokemón não existe ou falta conexão à internet");
+        }
+
+        const dados = await requisicao.json();
+
+        const foto = dados.sprites.other['official-artwork'].front_default;
+
+        paragrafoNome.innerHTML = `Nome: ${dados.name}`;
+        paragrafoId.innerHTML = `Id: ${dados.id}`;
+        imagemPokemon.setAttribute('src', foto);
+
     } catch (error) {
-        
+        console.error(error);
     }
 }
 
